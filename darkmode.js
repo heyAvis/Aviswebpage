f88afaf326b90fef6ae2b7341f2c19aa78669132
heyAvis <vicentemisolasboragay@gmail.com>
2023-12-18 18:27:41 +0800

    fixed bugs and added logo

--- darkmode.js
+++ darkmode.js
@@ -2,7 +2,7 @@
 
 let colors = document.querySelector(':root');
 let icon = document.getElementById('darkmodeIcon')
-
+let logo = document.getElementsByClassName('logo')
 
 
 function setColors() {
@@ -16,6 +16,7 @@ function setColors() {
         localStorage.setItem("mode", "light")
         document.getElementById('darkmodeIcon').className = "fa-solid fa-sun"
         document.getElementById('darkmodeText').innerHTML = "Dark mode"
+        document.getElementById('mylogo').src = "./src/images/avis-logo - black.png"
     }
     else {
         colors.style.setProperty('--textColor', '#cecece');
@@ -26,9 +27,10 @@ function setColors() {
         localStorage.setItem("mode", "dark")
         document.getElementById('darkmodeIcon').className = "fa-solid fa-moon"
         document.getElementById('darkmodeText').innerHTML = "Light mode"
-
-
+        document.getElementById('mylogo').src = "./src/images/avis-logo - green.png"
     }
+
+    
 
 }
 
@@ -40,15 +42,19 @@ function loadColors() {
         colors.style.setProperty('--backgroundColor', '#031e15');
         colors.style.setProperty('--backgroundColorHover', 'rgba(177, 248, 225, 0.1)');
         colors.style.setProperty('--buttonTextColor', 'black');
+        document.getElementById('mylogo').src = "./src/images/avis-logo - green.png"
 
         if (document.getElementById('darkmodeIcon').className === "fa-solid fa-sun") {
             document.getElementById('darkmodeIcon').className = "fa-solid fa-moon"
             document.getElementById('darkmodeText').innerHTML = "Light mode"
+           
+            
         }
 
         else {
             document.getElementById('darkmodeText').innerHTML = "Light mode"
             document.getElementById('darkmodeIcon').className = "fa-solid fa-moon"
+           
         }
 
     }
@@ -58,14 +64,18 @@ function loadColors() {
         colors.style.setProperty('--backgroundColor', 'white');
         colors.style.setProperty('--backgroundColorHover', 'rgba(232, 254, 247, 0.88)');
         colors.style.setProperty('--buttonTextColor', 'white');
+        document.getElementById('mylogo').src = "./src/images/avis-logo - black.png"
+        
         if (document.getElementById('darkmodeIcon').className === "fa-solid fa-moon") {
             document.getElementById('darkmodeIcon').className = "fa-solid fa-sun"
             document.getElementById('darkmodeText').innerHTML = "Dark mode"
+           
         }
 
         else {
             document.getElementById('darkmodeText').innerHTML = "Dark mode"
             document.getElementById('darkmodeIcon').className = "fa-solid fa-sun"
+           
         }
 
 
